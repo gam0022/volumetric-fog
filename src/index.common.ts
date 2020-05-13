bce38a21a6f5a6bc8abe3712a914563daec2d20c
gam0022 <gam0022@gmail.com>
2020-05-14 02:12:34 +0900

    ESLint auto fix: index.common.ts

--- src/index.common.ts
+++ src/index.common.ts
@@ -1,11 +1,21 @@
-import { Chromatiq } from "./chromatiq"
-import { mix, clamp, saturate, Vector3, remap, remapFrom, remapTo, easeInOutCubic, easeInOutCubicVelocity } from "./math"
+import { Chromatiq } from "./chromatiq";
+import {
+  mix,
+  clamp,
+  saturate,
+  Vector3,
+  remap,
+  remapFrom,
+  remapTo,
+  easeInOutCubic,
+  easeInOutCubicVelocity,
+} from "./math";
 
 // for Webpack DefinePlugin
-declare var PRODUCTION: boolean;
+declare let PRODUCTION: boolean;
 
 export const chromatiq = new Chromatiq(
-  109.714285714,// デモの長さ（秒）
+  109.714285714, // デモの長さ（秒）
   require("./shaders/build-in/vertex.glsl").default,
 
   // Image Shaders
@@ -28,20 +38,20 @@ export const chromatiq = new Chromatiq(
   require("./shaders/sound-resimulated.glsl").default,
 
   // Text Texture
-  gl => {
+  (gl) => {
     const canvas = document.createElement("canvas");
     const textCtx = canvas.getContext("2d");
     // window.document.body.appendChild(canvas);
 
     // MAX: 4096 / 128 = 32
     const texts = [
-            /* 0 */ "A 64K INTRO",
-            /* 1 */ "GRAPHICS",
-            /* 2 */ "gam0022",
-            /* 3 */ "MUSIC",
-            /* 4 */ "sadakkey",
-            /* 5 */ "RE: SIMULATED",
-            /* 6 */ "REALITY",
+      /* 0 */ "A 64K INTRO",
+      /* 1 */ "GRAPHICS",
+      /* 2 */ "gam0022",
+      /* 3 */ "MUSIC",
+      /* 4 */ "sadakkey",
+      /* 5 */ "RE: SIMULATED",
+      /* 6 */ "REALITY",
 
       // 7
       "MERCURY",
@@ -60,7 +70,7 @@ export const chromatiq = new Chromatiq(
       "FMS_Cat",
 
       // 15-20
-      String.fromCharCode(0x00BD) + "-bit Cheese",
+      String.fromCharCode(0x00bd) + "-bit Cheese",
       "Prismbeings",
       "0x4015 & YET1",
       "LJ & Alcatraz",
@@ -135,18 +145,22 @@ const Planets = {
   FMSCAT: 3 as const,
   MIX_B: 4 as const,
   EARTH: 5 as const,
-}
+};
 
-export const animateUniforms = (time: number, debugCamera: boolean, debugDisableReset: boolean) => {
+export const animateUniforms = (
+  time: number,
+  debugCamera: boolean,
+  debugDisableReset: boolean
+) => {
   const bpm = 140;
-  const beat = time * bpm / 60;
+  const beat = (time * bpm) / 60;
 
   let camera = new Vector3(0, 0, 10);
   let target = new Vector3(0, 0, 0);
-  let ball = new Vector3(0, 0, -10);
+  const ball = new Vector3(0, 0, -10);
 
   // reset values
-  chromatiq.uniformArray.forEach(uniform => {
+  chromatiq.uniformArray.forEach((uniform) => {
     // debug時は値の毎フレームリセットをしない
     if (!PRODUCTION) {
       if (debugDisableReset) return;
@@ -156,21 +170,31 @@ export const animateUniforms = (time: number, debugCamera: boolean, debugDisable
     chromatiq.uniforms[uniform.key] = uniform.initValue;
   });
 
-  new Timeline(beat).then(8 * 6, t => {
-    chromatiq.uniforms.gTonemapExposure = 1;
+  new Timeline(beat)
+    .then(8 * 6, (t) => {
+      chromatiq.uniforms.gTonemapExposure = 1;
 
-    camera = new Vector3(0.042533747248285514, 3.580181344478862, -3.2355081018643443).add(Vector3.fbm(t).scale(0.01));
-    target = new Vector3(0.7576763789243015, 3.4515422110479044, -0.21633410393024527);
+      camera = new Vector3(
+        0.042533747248285514,
+        3.580181344478862,
+        -3.2355081018643443
+      ).add(Vector3.fbm(t).scale(0.01));
+      target = new Vector3(
+        0.7576763789243015,
+        3.4515422110479044,
+        -0.21633410393024527
+      );
 
-    chromatiq.uniforms.gCameraFov = 37.88049605411499;
-    chromatiq.uniforms.gMandelboxScale = 2.88621239103946;
-    chromatiq.uniforms.gCameraLightIntensity = 0;
-    chromatiq.uniforms.gEmissiveIntensity = 0;
-    chromatiq.uniforms.gSceneEps = 0.0007057137501171619;
-  }).over(t => {
-    // デモ終了後
-    chromatiq.uniforms.gBlend = -1;
-  });
+      chromatiq.uniforms.gCameraFov = 37.88049605411499;
+      chromatiq.uniforms.gMandelboxScale = 2.88621239103946;
+      chromatiq.uniforms.gCameraLightIntensity = 0;
+      chromatiq.uniforms.gEmissiveIntensity = 0;
+      chromatiq.uniforms.gSceneEps = 0.0007057137501171619;
+    })
+    .over((t) => {
+      // デモ終了後
+      chromatiq.uniforms.gBlend = -1;
+    });
 
   if (!PRODUCTION && debugCamera) {
     return;
@@ -182,4 +206,4 @@ export const animateUniforms = (time: number, debugCamera: boolean, debugDisable
   chromatiq.uniforms.gCameraTargetX = target.x;
   chromatiq.uniforms.gCameraTargetY = target.y;
   chromatiq.uniforms.gCameraTargetZ = target.z;
-}
+};
